f8f6b141d211b12153296e73413a9a2e25e7165e
acaceres <adnanhector1997@gmail.com>
2023-08-30 13:40:59 -0500

    madificado

--- obtener_combinacion_parametro.py
+++ obtener_combinacion_parametro.py
@@ -31,35 +31,39 @@ def main():
     # Convertir la columna 'datafetd' a tipo datetime
     df['fecha_faltante'] = pd.to_datetime(df['fecha_faltante'])
 
-    # Obtener la lista de estaciones únicas
+    # Obtener la lista de estaciones y combinaciones de parámetros únicos
     unique_stations = df['id_estacion'].unique()
+    unique_combinations = df['id_combinacion_parametro'].unique()
 
     for station in unique_stations:
-        station_df = df[df['id_estacion'] == station]
+        for combination in unique_combinations:
+            station_combination_df = df[(df['id_estacion'] == station) & (df['id_combinacion_parametro'] == combination)]
 
-        # Convertir la columna 'fecha_faltante' en un índice para facilitar la comparación
-        station_df.set_index('fecha_faltante', inplace=True)
+            # Verificar si hay datos antes de calcular las fechas faltantes
+            if not station_combination_df.empty:
+                # Convertir la columna 'fecha_faltante' en un índice para facilitar la comparación
+                station_combination_df.set_index('fecha_faltante', inplace=True)
 
-        # Crear un rango de fechas completo para esa estación con la frecuencia adecuada
-        date_range = pd.date_range(start=station_df.index.min(), end=station_df.index.max(), freq='T')
+                # Crear un rango de fechas completo para esa estación y combinación con la frecuencia adecuada
+                date_range = pd.date_range(start=station_combination_df.index.min(), end=station_combination_df.index.max(), freq='T')
 
-        # Encontrar las fechas faltantes
-        missing_dates = date_range[~date_range.isin(station_df.index)]
+                # Encontrar las fechas faltantes
+                missing_dates = date_range[~date_range.isin(station_combination_df.index)]
 
-        # Crear un DataFrame de fechas faltantes con todas las columnas necesarias
-        missing_dates_df = pd.DataFrame(data={'fechas_faltantes': missing_dates})
-        missing_dates_df['id_estacion'] = station_df['id_estacion'][0]
-        missing_dates_df['nemonico'] = station_df['nemonico'][0]
-        missing_dates_df['id_combinacion_parametro'] = station_df['id_combinacion_parametro'][0]
+                # Crear un DataFrame de fechas faltantes con todas las columnas necesarias
+                missing_dates_df = pd.DataFrame(data={'fechas_faltantes': missing_dates})
+                missing_dates_df['id_estacion'] = station_combination_df['id_estacion'][0]
+                missing_dates_df['nemonico'] = station_combination_df['nemonico'][0]
+                missing_dates_df['id_combinacion_parametro'] = station_combination_df['id_combinacion_parametro'][0]
 
-        # Si hay fechas faltantes, crear directorio si no existe y guardar el DataFrame en un archivo CSV
-        if not missing_dates_df.empty:
-            output_dir = f'estacion_{station}'
-            os.makedirs(output_dir, exist_ok=True)
+                # Si hay fechas faltantes, crear directorio si no existe y guardar el DataFrame en un archivo CSV
+                if not missing_dates_df.empty:
+                    output_dir = f'estacion_{station}'
+                    os.makedirs(output_dir, exist_ok=True)
 
-            csv_filename = f'{output_dir}/estacion_{station}.csv'
-            missing_dates_df.reset_index(inplace=True)
-            missing_dates_df.to_csv(csv_filename, index=False, columns=['id_estacion', 'nemonico', 'id_combinacion_parametro', 'fechas_faltantes'])
+                    csv_filename = f'{output_dir}/estacion_{station}_combinacion_{combination}.csv'
+                    missing_dates_df.reset_index(inplace=True)
+                    missing_dates_df.to_csv(csv_filename, index=False, columns=['id_estacion', 'nemonico', 'id_combinacion_parametro', 'fechas_faltantes'])
 
 if __name__ == "__main__":
     main()
